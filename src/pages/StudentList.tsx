9515eb106775fdf08e7156b94ad53e799758d779
Kevin Dang <kevin.kd.dang@gmail.com>
2024-11-29 13:39:43 -0500

    import changes

--- src/pages/StudentList.tsx
+++ src/pages/StudentList.tsx
@@ -1,10 +1,9 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
 import { useAuth } from "../hooks/useAuth";
 import { StudentCard } from "../components/StudentCard/StudentCard";
 import { Student } from "../types";
 import { getStudents } from "../services/dac-api";
-axios.defaults.withCredentials = true;
+
 
 export const StudentList = () => {
   const [students, setStudents] = useState<Student[]>([]);
